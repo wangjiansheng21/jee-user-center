9ca08e0e475f493605dfae3590ead6bc06f8c9b9
jiansheng.wang <wjs123456>
2021-03-10 16:43:20 +0800

    授权登录

--- jee-dubbo-common/src/main/java/org/jee/boot/dubbo/ReponseCodeEnum.java
+++ jee-dubbo-common/src/main/java/org/jee/boot/dubbo/ReponseCodeEnum.java
@@ -3,7 +3,9 @@ package org.jee.boot.dubbo;
 public enum ReponseCodeEnum {
     SYS_EXCEPTION("500", "系统异常"),
     SYS_BUSY("600", "系统繁忙"),
-    SYS_FAIL("700", "调用失败");
+    SYS_FAIL("700", "调用失败"),
+    TOKEN_EXPIRE("800", "登录过期"),
+    TOKEN_LOST("900", "token缺失");
     private String code;
 
     private String msg;
--- jee-dubbo-common/src/main/java/org/jee/boot/dubbo/response/RpcResponse.java
+++ jee-dubbo-common/src/main/java/org/jee/boot/dubbo/response/RpcResponse.java
@@ -9,11 +9,11 @@ import java.io.Serializable;
 @Data
 public class RpcResponse<T> implements Serializable {
 
-    private String code="200";
+    private String code = "200";
 
-    private String msg="成功";
+    private String msg = "成功";
 
-    private Long total=0L;
+    private Long total = 0L;
 
     private T data;
 
@@ -50,40 +50,48 @@ public class RpcResponse<T> implements Serializable {
     }
 
     //系统调用失败
-    public void setSysFail(String msg){
-        this.code= ReponseCodeEnum.SYS_FAIL.getCode();
-        this.msg=ReponseCodeEnum.SYS_FAIL.getMsg();
-        if(!StringUtils.isEmpty(msg)){
-            this.msg=msg;
+    public void setSysFail(String msg) {
+        this.code = ReponseCodeEnum.SYS_FAIL.getCode();
+        this.msg = ReponseCodeEnum.SYS_FAIL.getMsg();
+        if (!StringUtils.isEmpty(msg)) {
+            this.msg = msg;
         }
     }
 
     //系统异常
-    public void setSysException(){
-        this.code= ReponseCodeEnum.SYS_EXCEPTION.getCode();
-        this.msg=ReponseCodeEnum.SYS_EXCEPTION.getMsg();
+    public void setSysException() {
+        this.code = ReponseCodeEnum.SYS_EXCEPTION.getCode();
+        this.msg = ReponseCodeEnum.SYS_EXCEPTION.getMsg();
     }
 
     //系统繁忙
-    public void setSysBusy(){
-        this.code= ReponseCodeEnum.SYS_BUSY.getCode();
-        this.msg=ReponseCodeEnum.SYS_BUSY.getMsg();
+    public void setSysBusy() {
+        this.code = ReponseCodeEnum.SYS_BUSY.getCode();
+        this.msg = ReponseCodeEnum.SYS_BUSY.getMsg();
     }
 
-    public Boolean isSucces(){
-        if(code.equals("200")){
+    public static RpcResponse build(ReponseCodeEnum reponseCodeEnum) {
+        RpcResponse rpcResponse = new RpcResponse();
+        rpcResponse.code = reponseCodeEnum.getCode();
+        rpcResponse.msg = reponseCodeEnum.getMsg();
+        return rpcResponse;
+    }
+
+
+    public Boolean isSucces() {
+        if (code.equals("200")) {
             return true;
         }
         return false;
     }
 
-    public void copy(RpcResponse rpcResponse){
-        code=rpcResponse.getCode();
-        msg=rpcResponse.getMsg();
+    public void copy(RpcResponse rpcResponse) {
+        code = rpcResponse.getCode();
+        msg = rpcResponse.getMsg();
     }
 
-    public static  RpcResponse ok(){
-        return  new RpcResponse();
+    public static RpcResponse ok() {
+        return new RpcResponse();
     }
 
 
